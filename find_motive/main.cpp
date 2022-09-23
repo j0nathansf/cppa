0522346fbf64bd0068f617345a12714e0b85bdf8
Jonathan <jonathan.ren@protonmail.com>
2022-09-23 11:35:46 +0200

    find motive

---
+++ find_motive/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+#include <string.h>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+    string file_path = argv[1];
+    string motive = argv[2];
+    int nb = 0;
+    string word;
+    fstream file;
+    file.open(file_path);
+    if (!file)
+    {
+        cerr << "The file " << file_path << " could not be opened.\n";
+        return -1;
+    }
+    while (file >> word)
+    {
+        if (word.compare(motive) == 0)
+        {
+            nb += 1;
+        }
+    }
+    cout << "The file " << file_path << " contains " << nb << " words containing the motive " << motive << "\n";
+    file.close();
+    return 0;
+}
