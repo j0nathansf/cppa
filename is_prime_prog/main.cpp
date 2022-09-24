de5ec1fc14146412419803a5b1a1989761a9542e
Jonathan <jonathan.ren@protonmail.com>
2022-09-24 17:20:47 +0200

    add cmakelist

--- is_prime_prog/main.cpp
+++ is_prime_prog/main.cpp
@@ -1,19 +1,28 @@
 #include <iostream>
 #include <stdlib.h>
 
-std::string check_prime(int nb) {
-    for (int i = 2; i <= nb / 2; i++) {
+std::string check_prime(int nb)
+{
+    if (nb == 1)
+    {
+        return "False";
+    }
+    for (int i = 2; i <= nb / 2; i++)
+    {
         if (nb %i != 0)
         {
             continue;
-        } else {
+        }
+        else
+        {
             return "False";
         }
     }
     return "True";
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char** argv)
+{
     for (int i = 1; i < argc; i++)
     {
         std::cout << argv[i] << " is a prime: " << check_prime(atoi(argv[i])) << "\n";
