c047f3e5d780d59ec90a5a15b76c857b800007e6
Jonathan <jonathan.ren@protonmail.com>
2022-09-24 17:15:39 +0200

    add cmakelist

--- find_motive/main.cpp
+++ find_motive/main.cpp
@@ -5,6 +5,10 @@
 using namespace std;
 
 int main(int argc, char **argv) {
+    if (argc < 2)
+    {
+        return 1;
+    }
     string file_path = argv[1];
     string motive = argv[2];
     int nb = 0;
@@ -14,7 +18,7 @@ int main(int argc, char **argv) {
     if (!file)
     {
         cerr << "The file " << file_path << " could not be opened.\n";
-        return -1;
+        return 1;
     }
     while (file >> word)
     {
